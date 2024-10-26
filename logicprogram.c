7580e4a0180f34e186c74bf13b60139dc0ba0201
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-10-26 20:17:46 +0530

    logical

--- logicprogram.c
+++ logicprogram.c
@@ -5,6 +5,11 @@ int main()
     int a = 10;
     int b = 30;
 
-    printf("a>b=%d\n", a > b); // true or false
+    printf("a>b =%d\n", a > b);    // true or false
+    printf("a<b =%d\n", a < b);    // true or false
+    printf("a>=b = %d\n", a >= b); // true or false
+    printf("a<=b = %d\n", a <= b); // true or false
+    printf("!a=%d\n", !a);         // true or false
+    printf("!b=%d\n", !b);         // true or false
     return 0;
 }
