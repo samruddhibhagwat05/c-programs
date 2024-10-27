ff826c798888922c5b29d6b57320899c19d7f09a
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-10-27 17:49:21 +0530

    Update Assignmentoperator.c

--- Assignmentoperator.c
+++ Assignmentoperator.c
@@ -5,10 +5,7 @@ int main()
     int a = 10;
     int b = 20;
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> 5fe08d53a416ca38fc491ebfb9c72c29d8a2f6d9
+
     printf("sum = %d\n", a += b); // addition of two numbers
 
     printf("subtraticon = %d\n", a -= b); // subtraction of two number
