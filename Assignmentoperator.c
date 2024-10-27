dbf5dd525916a6f41749b565d25719b07c91da7d
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-10-27 11:53:39 +0530

    assignment operator

---
+++ Assignmentoperator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    // Corrected printf statement
+    printf("sum = %d\n", a += b); // addition of two numbers
+
+    printf("subtraticon = %d\n", a -= b); // subtraction of two number
+
+    printf("multiplication = %d\n", a *= b); // multiplication of two numbers
+
+    printf("division = %d\n", a /= b); // quoient of two numbers
+
+    printf("remainder = %d\n", a %= b); // remainder of two numbers
+
+    return 0;
+}
