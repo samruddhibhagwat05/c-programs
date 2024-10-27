19e9e9a48003d5331e5da238f0dd653de30570ce
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-10-27 12:10:00 +0530

    Bitwise operator

---
+++ bitwiseoperator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5; // 5=0101
+    int b = 3; // 3=0011
+
+    printf("a&b:%d\n", a & b); // Result:1 (0001)
+
+    printf("a|b:%d\n", a | b); // Result:7 (o111)
+
+    printf("a^b:%d\n", a ^ b); // Reusult:6 (0110)
+
+    printf("-a:%d\n", a); // Reusult:-6 (11111010)
+
+    printf("a<<1:%d\n", a << 1); // Reusult:10 (1010)
+
+    printf("a>>1:%d\n", a >> 1); // Reusult:2 (0010)
+
+    return 0;
+}
