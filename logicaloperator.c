5fe08d53a416ca38fc491ebfb9c72c29d8a2f6d9
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-10-27 13:48:25 +0530

    Update logicaloperator.c

--- logicaloperator.c
+++ logicaloperator.c
@@ -5,13 +5,13 @@ int main()
     int a = 10;
     int b = 20;
 
-    printf("a&&b: %d\n", a && b); // Logical AND: returns true(1) if both a and b are true (non-zero)
+    printf("a&&b: %d\n", a && b); // Logical AND: (1&&0) is true if both are non zero
 
-    printf("a||b: %d\n", a || b); // Logical OR: returns true(1) if atleat one a or b is true (non-zero)
+    printf("a||b: %d\n", a || b); // Logical OR: (1||0) is true if atleast one of them is non zero
 
-    printf("!a: %d\n", !a); // Logical NOT: returns true (1) if a is zero, otherwise returns false (0)
+    printf("!a: %d\n", !a); // Logical NOT: true if a is zero, otherwise returns false (0)
 
-    printf("!b: %d\n", !b); // Logical NOT: returns true (1) if b is zero, otherwise returns false (0)
+    printf("!b: %d\n", !b); // Logical NOT:  true (1) if b is zero, otherwise returns false (0)
 
     return 0;
 }
