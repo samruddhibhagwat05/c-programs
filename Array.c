29b75f02fefa99fea1343b97ce9f53addc227584
samruddhibhagwat05 <bhagwatsamruddhi05@gmail.com>
2024-11-10 18:54:39 +0530

    array.c

--- Array.c
+++ Array.c
@@ -2,19 +2,16 @@
 
 int main()
 {
-    int x[10] = {1, 2, 3, 4, 5, 6};
     int i;
+    int x[10] = {1, 2, 3, 4, 5, 6};
 
-    // Initialize or assign values to the elements as needed
     x[0] = 1;
     x[1] = 2;
-    x[3] = 5;
+    x[3] = 6;
 
-    // Print the elements in the array
     for (i = 0; i < 10; i++)
     {
         printf("%d ", x[i]);
     }
-
     return 0;
 }
